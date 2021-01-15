e6b3d7a4904cf2401fface48fc0c9638034260ec
SalonTati <manukyant@mail.ru>
2021-01-15 04:55:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+
+
+var popup = this.document.getElementsByClassName("service-header");
+// for (var i = 0; i < popup.length; i++) {
+//     popup[i].onclick = function() {
+//         if (this.parentElement.classList.contains('open')) {
+//             this.parentElement.classList.remove('open');
+//         } else {
+//             for (var i = 0; i < popup.length; i++) {
+//                 if (popup[i].parentElement.classList.contains('open')) {
+//                     popup[i].parentElement.classList.remove('open');
+//                 }
+//             }
+//             this.parentElement.classList.add('open');
+//             window.scrollTo(0, this.offsetTop-72);
+//         }
+//     };
+// }
+}
